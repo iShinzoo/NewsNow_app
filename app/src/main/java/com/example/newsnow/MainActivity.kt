2aac7670a8503e51788fff0117d081037fab3b32
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2024-02-21 23:48:43 +0530

    splash screen

--- app/src/main/java/com/example/newsnow/MainActivity.kt
+++ app/src/main/java/com/example/newsnow/MainActivity.kt
@@ -3,44 +3,32 @@ package com.example.newsnow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.newsnow.ui.theme.NewsNowTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             NewsNowTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+
+                Box(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    contentAlignment = Alignment.Center
                 ) {
-                    Greeting("Android")
+                    
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NewsNowTheme {
-        Greeting("Android")
     }
 }
